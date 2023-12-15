fe1f214093cbce68c680877811a22b5982fc1402
Bex Ross <bexeross@googlemail.com>
2023-12-15 10:47:30 +0100

    Start point from deepseaNiN
    Will be modifying and adding from here by copying from there but cleaning as I go

---
+++ Config.R
@@ -0,0 +1,13 @@
+
+## Paths
+
+#library(readxl)
+# https://owncloud.imr.no/index.php/s/lCkOERjzJkPItZj
+
+# C:\Users\genoveva\ownCloud\NiN_Mareano\inputs
+
+#dataPath <- "C:/Users/genoveva/OneDrive - Havforskningsinstituttet/General" # geno's path
+dataPath<- "C:/Users/a21448/Havforskningsinstituttet/NiN + MAREANO - General" # bex's path
+#datapath <- "C:/Users/Wilson_Margaret/Havforskningsinstituttet/NiN + MAREANO - General" # Margaret's path
+
+dataPath_2 <- "C:/Users/a21448/Havforskningsinstituttet/Video analysis lab - Reference List"
